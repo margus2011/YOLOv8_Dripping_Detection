179009a26408f01dd70777854cd0cbc13f924d11
Jeevadithyan Pommiah Sivakumar <jeevadithyan@gmail.com>
2024-06-26 17:25:56 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# YOLO_Dripping_Detection
+ A repository used for detecting dripping during laser aided additive manufacturing.
---
+++ train_model.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+
+# Load a model
+model= YOLO("yolov8n.yaml") 
+
+# Use the model
+results = model.train(data="C:/Users/jeeva/OneDrive/Documents/YOLO/config.yaml", epochs=10) 
